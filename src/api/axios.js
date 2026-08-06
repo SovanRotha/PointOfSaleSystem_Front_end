5a62bd0f3788745cdcf0a5cd26269baf70900107
sovanrotha.pit <sovanrothapit@gmail.com>
2026-08-07 00:52:30 +0700

    deploy 1

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8000",
+    baseURL: "https://pointofsale-sk8k.onrender.com",
     withCredentials: true,
     withXSRFToken: true,
     headers: {
